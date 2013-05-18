753151589fa9cbc51b1fe8627f66c890567825cb
Piotr.Modzelewski <piotr.modzelewski@allegro.pl>
2013-05-19 00:18:06 +0200

    refactor + first opt

--- mvnhlp/mvnhlp.pl
+++ mvnhlp/mvnhlp.pl
@@ -1,20 +1,24 @@
 #!/usr/bin/perl
 use XML::Simple;
 use Data::Dumper;
-use Getopt::Std;
+use Getopt::Long;
+
+my $pickedEnv;
+
+GetOptions("env=s" => \$pickedEnv);
 
 my $xml = new XML::Simple;
-my $data = $xml->XMLin("mvnhlp.xml");
-
-print Dumper(getEnvsData($data));
-
-
-sub getEnvsData {
-	my $result = {};
-	my $envs = $_[0]->{envs}->{env};
-	while ( ($envName, $env) = each $envs ){		
-		my @tmp = $env->{profiles}->{profile};		
-		$result->{$envName} = \@tmp;		
-	}
-	return $result;
+my $data = $xml->XMLin("mvnhlp.xml", ForceArray => 1, KeyAttr    => {});
+my $envs = $data->{envs}[0]->{env};
+my $config = {};
+foreach my $env (@$envs) {
+	$config->{$env->{name}} = $env->{profiles}[0]->{profile};	
 }
+
+print "Picked env: $pickedEnv, using profiles:\n" ;
+my $pickedProfiles = $config->{$pickedEnv};
+
+foreach my $profile (@$pickedProfiles){
+	print "* $profile \n";
+}
+
