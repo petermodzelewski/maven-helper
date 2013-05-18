c3ca462089141bb03b01d285cc805cbb52dbe6ac
Piotr.Modzelewski <piotr.modzelewski@allegro.pl>
2013-05-19 00:23:19 +0200

    error when not existing profile

--- mvnhlp/mvnhlp.pl
+++ mvnhlp/mvnhlp.pl
@@ -3,16 +3,23 @@ use XML::Simple;
 use Data::Dumper;
 use Getopt::Long;
 
+my $xmlFile = "mvnhlp.xml";
+
 my $pickedEnv;
 
 GetOptions("env=s" => \$pickedEnv);
 
 my $xml = new XML::Simple;
-my $data = $xml->XMLin("mvnhlp.xml", ForceArray => 1, KeyAttr    => {});
+my $data = $xml->XMLin($xmlFile, ForceArray => 1, KeyAttr    => {});
 my $envs = $data->{envs}[0]->{env};
 my $config = {};
 foreach my $env (@$envs) {
 	$config->{$env->{name}} = $env->{profiles}[0]->{profile};	
+}
+
+if (! exists $config->{$pickedEnv}) {
+	print "$pickedEnv not configured, check configuration in $xmlFile";
+	exit 1;
 }
 
 print "Picked env: $pickedEnv, using profiles:\n" ;
