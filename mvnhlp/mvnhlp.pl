20ea997677aa173c2b926eae223de8eb1fe9882d
Piotr.Modzelewski <piotr.modzelewski@allegro.pl>
2013-05-19 18:43:00 +0200

    more configs and args

--- mvnhlp/mvnhlp.pl
+++ mvnhlp/mvnhlp.pl
@@ -4,12 +4,23 @@ use Data::Dumper;
 use Getopt::Long;
 
 my $xmlFile = "mvnhlp.xml";
-
 my $pickedEnv;
+my $pickedApp;
 my $help;
 
-GetOptions("env=s" => \$pickedEnv, "help" => \$help);
+### Reading configuration ###
 
+my $xml = new XML::Simple;
+my $data = $xml->XMLin($xmlFile, ForceArray => 1, KeyAttr    => {});
+$envsConfig = parseEnvs($data);
+$appsConfig = parseApps($data);
+
+print Dumper($appsConfig);
+
+### Handling arguments ###
+GetOptions(	"env=s" => \$pickedEnv, 
+			"app=s" => \$pickedApp,
+			"help" => \$help);
 if($help) {
 	printHelp();
 }
@@ -18,18 +29,15 @@ if(!$pickedEnv){
 	argsError("--env");	
 }
 
+if(!$pickedApp) {
+	argsError("--app");
+}
 
-
-my $xml = new XML::Simple;
-my $data = $xml->XMLin($xmlFile, ForceArray => 1, KeyAttr    => {});
-
-
-
-$envsConfig = parseEnvs($data);
 if (! exists $envsConfig->{$pickedEnv}) {
     configError("$pickedEnv not configured"); 			
 }
 
+### WORK HERE ###
 print "Picked env: $pickedEnv, using profiles:\n" ;
 my $pickedProfiles = $envsConfig->{$pickedEnv};
 
@@ -37,6 +45,7 @@ foreach my $profile (@$pickedProfiles){
 	print "* $profile \n";
 }
 
+### Helper functions ###
 
 sub parseEnvs {
 	my $envs = $_[0]->{envs}[0]->{env};
@@ -45,6 +54,19 @@ sub parseEnvs {
 	foreach my $env (@$envs) {
 		$config->{$env->{name}} = $env->{profiles}[0]->{profile};	
 	}	
+	return $config;
+}
+
+sub parseApps {
+	my $apps = $_[0]->{applications}[0]->{application};
+	my $config = {};
+	foreach my $app (@$apps) {
+		$appConfig = {};
+		$appConfig->{name} = $app->{name};
+		$appConfig->{pom} = $app->{pom}[0];
+		$appConfig->{envs} = parseEnvs($app);
+		$config->{$appConfig->{name}} = $appConfig;
+	}
 	return $config;
 }
 
@@ -67,5 +89,6 @@ sub printHelp {
 	print "Parameters:\n";
 	print "\t --env=ENV -e ENV: to specify environment to target\n";
 	print "\t --help -h: shows this message\n";
+	print "\t --app=APP_NAME -a APP_NAME: to specify application to work on";
 	exit 1;
 }
